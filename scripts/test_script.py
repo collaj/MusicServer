7dbba0166bc8184cf2f616e9ac4ebca5251c875e
James Colla <collajames@gmail.com>
2015-10-07 00:43:45 -0400

    Adds python script for updating database and dependency text document -
    Adds the python script that will run in the background of the server with
    filewatcher to update the database as songs are added and deleted from the
    server
    Adds a python script used for testing the main script
    Adds a text document outlining the dependencies for this project

---
+++ scripts/test_script.py
@@ -0,0 +1,13 @@
+﻿import os
+import time
+
+os.chdir("F:\\Dropbox\\MusicServer\\script_testing")
+walk = os.walk("C:\\Users\\Zippy\\Desktop\\test songs")
+FSEVENT = "new"
+for item in walk:
+    FILEPATHPREFIX = item[0] + "\\"
+    for song in item[2]:
+        if song.endswith(".mp3"):
+            FILEPATH = FILEPATHPREFIX + song
+            os.system('python script.py "' + song + '" "' + FILEPATH + '" "' + FSEVENT + '"')
+            #time.sleep(250)
